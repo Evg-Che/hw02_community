013638bdf7c24ca5e68d735f0d45e5978d3d1268
Евгения Черноусова <evgesha.che@yandex.ru>
2023-02-01 22:15:26 +0300

    Fix3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -14,7 +14,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.group.all()[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
